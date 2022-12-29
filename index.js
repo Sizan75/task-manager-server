e7662bd35f5468c32796bc06388cb7d11c8e3652
Sizan75 <sakiuzzaman26@gmail.com>
2022-12-29 18:15:58 +0600

    task delete done

--- index.js
+++ index.js
@@ -28,6 +28,27 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/mytasks',async(req,res)=>{
+       
+            let query = {};
+    
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = taskCollection.find(query);
+            const mytask = await cursor.toArray();
+            res.send(mytask);
+        })
+    
+        app.delete('/tasks/:id',async(req,res)=>{
+            const id=req.params.id
+            const query={_id: ObjectId(id)}
+            const result = await taskCollection.deleteOne(query)
+            res.send(result)
+           }) 
+
     }
     finally{
     
